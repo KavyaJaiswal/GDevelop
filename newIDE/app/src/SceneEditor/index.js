a390bf50db70756fb3fda80d30cf55f64d7cecec
Florian Rival <Florian.Rival@gmail.com>
2020-05-27 21:17:55 +0200

    Refactor changes cancelling of GDevelop.js objects
    * Use a typed hook (shorter and type-safe to use)
    * Avoid the necessity of providing a function to create an object.
    * Only unserialize back to the object if cancelling changes (instead of when applying).

--- newIDE/app/src/SceneEditor/index.js
+++ newIDE/app/src/SceneEditor/index.js
@@ -1096,7 +1096,6 @@ export default class SceneEditor extends React.Component<Props, State> {
         {!!this.state.editedGroup && (
           <ObjectGroupEditorDialog
             project={project}
-            open
             group={this.state.editedGroup}
             objectsContainer={layout}
             globalObjectsContainer={project}
